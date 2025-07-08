1acf528328352541b31d6851a8824abbfe5992e5
Rohit Joshi <rohit250104joshi@gmail.com>
2025-07-08 21:58:32 +0530

    js added

---
+++ StonePaperScissor/script.js
@@ -0,0 +1,61 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+
+let cpuScore = document.querySelector(".computer-score");
+let userScore = document.querySelector(".user-score");
+
+let choices = document.querySelectorAll(".choiceBtn");
+
+let scoreForUser=0;
+let scoreForCpu=0;
+
+
+function winnerCpu(){
+    scoreForCpu++;
+    cpuScore.innerText=`${scoreForCpu}`;
+    document.querySelector(".result").innerText=`Cpu Wins`;
+
+}
+function winnerUser(){
+    scoreForUser++;
+    userScore.innerText=`${scoreForUser}`;
+    document.querySelector(".result").innerText=`User Wins`;
+}
+
+function winningPattern(userChoice,cpuChoice){
+
+    if(userChoice===cpuChoice){
+        document.querySelector(".result").innerText=`Draw`;
+    }
+    else if((userChoice === 1 && cpuChoice === 2) || (userChoice === 2 && cpuChoice ===3) || (userChoice === 3 && cpuChoice ===1)){
+        winnerCpu();
+    }else{
+        winnerUser();
+    }
+}
+
+
+
+
+    rock.addEventListener('click', e=>{
+        let userChoice = 1;
+        let cpuChoice = Math.floor(Math.random() * 3) + 1;
+        winningPattern(userChoice,cpuChoice);
+    });
+
+
+    paper.addEventListener('click', e=>{
+        let userChoice = 2;
+        let cpuChoice = Math.floor(Math.random() * 3) + 1;
+        winningPattern(userChoice,cpuChoice);
+    });
+
+
+    scissor.addEventListener('click', e=>{
+        let userChoice = 3;
+        let cpuChoice = Math.floor(Math.random() * 3) + 1;
+        winningPattern(userChoice,cpuChoice);
+    });
+
+
