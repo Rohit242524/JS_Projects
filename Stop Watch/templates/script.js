f7b00b9bc73ef2df33a8b753450e1ed69800e677
Rohit242524 <rohit250104joshi@gmail.com>
2025-08-13 01:07:42 +0530

    updated home page structure and stopwatch js

--- Stop Watch/templates/script.js
+++ Stop Watch/templates/script.js
@@ -1,6 +1,51 @@
 const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
 
-play.addEventListener("click",()=>{
-    console.log("123");
+const time = document.querySelector(".time");
+
+const reset = document.querySelector(".reset");
+const store = document.querySelector(".store");
+
+const btn = document.querySelectorAll(".btn");
+
+let startTime = 0;
+let elapsedTime = 0;
+let timeInterval;
+
+btn.forEach(element => {
+    if(element === play){
+        element.addEventListener("click",()=>{
+            console.log("play");
+            startTime = Date.now();
+            timerInterval = setInterval(startDisplayTime, 10);
+            play.style.display = "none";
+            pause.style.display = "flex";
+    });
+    }else if (element === pause){
+        element.addEventListener("click",()=>{
+            console.log("pause");
+            pause.style.display = "none";
+            play.style.display = "flex";
+        });
+    }else if(element === reset){
+        element.addEventListener("click",()=>{
+            console.log("reset");
+        });
+    }else if(element === store){
+        element.addEventListener("click",()=>{
+            console.log("store");
+        });
+    }
+
 });
+
+
+function startDisplayTime(){
+    let totalTime = Date.now() - startTime +  elapsedTime;
+
+    let minutes = Math.floor( (totalTime % 1000*60*60) / (1000*60));
+    let second =  Math.floor( (totalTime % 1000*60)/1000 );
+    let milliseconds = time % 1000;
+
+    time.innerText = {hello};
+}
